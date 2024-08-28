6d68137d731457dbda46a4b57f98495abf93aa93
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-08-28 18:41:45 +0200

    Added game.py

---
+++ game.py
@@ -0,0 +1,9 @@
+""" game.py contains the logic for the game. It contains a Game class which can be used to create a new game.
+"""
+# Import required modules
+
+"""pygame is used to carry out game-related things such as creating and managing the window, setting up game controls, 
+   and detecting any user event that happens while playing.
+"""
+import pygame
+pygame.init() # Init pygame to access its content
---
+++ main.py
@@ -0,0 +1,15 @@
+"""main.py is the starting point for the game.
+   It starts a first game, and when finished, it enters a loop which allows the player to play as many times as they want."""
+import pygame # Import pygame to ensure all required functionalities are present
+pygame.init() # Init pygame
+#import game # Import game.py, which is the script that handles the logic of the game
+from tkinter import messagebox # We'll use the messagebox module from tkinter to display dialog boxes to the player
+
+
+# Let's start a first game immediatly
+
+
+# After the first game has ended, enter a loop to allow the player to replay
+
+while True:
+    pass
